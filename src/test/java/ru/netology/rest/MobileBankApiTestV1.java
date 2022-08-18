d88ada617f79c595dce9fc5eef1f7fcf8392655d
Lira Bulatova <bulatova-lira@mail.ru>
2022-08-19 02:57:04 +0300

    тест на соответствие схеме прошел

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,8 +1,10 @@
 package ru.netology.rest;
 
 import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.*;
+
 
 class MobileBankApiTestV1 {
     @Test
@@ -16,6 +18,7 @@ class MobileBankApiTestV1 {
           .get("/demo/accounts")
       // Проверки
       .then()
-          .statusCode(200);
+          .statusCode(200)
+          .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
 }
